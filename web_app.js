4ef669e7f5a769472562b149aa34eb79010236b9
nastiko <anastasia.hrynkevich@outlook.com>
2022-03-25 00:02:48 +0000

    Change names classes. Make button clickable.

--- web_app.js
+++ web_app.js
@@ -1,43 +1,62 @@
+let currentStep = $('.form-block.show').index()+1;
+$('.nav-buttons .next').click(function () {
+    let elNow = $('.form-block.show');
+    let elNext = elNow.next();
+    if (elNext && elNext.length) {
+        elNow.removeClass("show");
+        elNext.addClass("show");
+        $('.calendar_info strong').html(`${++currentStep}/3`);
+    } else {
+        $(this).prop('disabled', true);
+    }
+});
+
+
+
+
+
+
 // init variables
-/* let eightBallReply = '';
-let userName = window.prompt("Enter your name: ");
-
-// short-hand syntax
-userName ? console.log(`Hello, ${userName}!`) : console.log(`Hello!`);
-
-
-// random number
-let randomNumber = Math.floor(Math.random()*8);
-console.log(`Random number will be ${randomNumber}`);
-
-switch (randomNumber) {
-    case 1:
-        eightBallReply = `It is certain`;
-        break;
-    case 2:
-        eightBallReply = `It is decidedly so`;
-        break;
-    case 3:
-        eightBallReply = `Reply hazy try again`;
-        break;
-    case 4:
-        eightBallReply = `Cannot predict now`;
-        break;
-    case 5:
-        eightBallReply = `Do not count on it`;
-        break;
-    case 6:
-        eightBallReply = `My sources say no`;
-        break;
-    case 7:
-        eightBallReply = `Outlook not so good`;
-        break;
-    case 8:
-        eightBallReply = `Signs point to yes`;
-        break;
-    default:
-        eightBallReply = `Try again`;
-        break;
-}
-console.log(`The Magic Eight Ball\'s answer is "${eightBallReply}".`);
- */
+// let eightBallReply = '';
+// let userName = window.prompt("Enter your name: ");
+//
+// // short-hand syntax
+// userName ? console.log(`Hello, ${userName}!`) : console.log(`Hello!`);
+//
+//
+// // random number
+// let randomNumber = Math.floor(Math.random()*8);
+// console.log(`Random number will be ${randomNumber}`);
+//
+// switch (randomNumber) {
+//     case 1:
+//         eightBallReply = `It is certain`;
+//         break;
+//     case 2:
+//         eightBallReply = `It is decidedly so`;
+//         break;
+//     case 3:
+//         eightBallReply = `Reply hazy try again`;
+//         break;
+//     case 4:
+//         eightBallReply = `Cannot predict now`;
+//         break;
+//     case 5:
+//         eightBallReply = `Do not count on it`;
+//         break;
+//     case 6:
+//         eightBallReply = `My sources say no`;
+//         break;
+//     case 7:
+//         eightBallReply = `Outlook not so good`;
+//         break;
+//     case 8:
+//         eightBallReply = `Signs point to yes`;
+//         break;
+//     default:
+//         eightBallReply = `Try again`;
+//         break;
+// }
+// console.log(`The Magic Eight Ball\'s answer is "${eightBallReply}".`);
+//
+// console.log(`The Magic Eight Ball\'s answer is 1.`);
