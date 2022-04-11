3ff7d6be0283050199a83fec67756ce0bf1bf57f
nastiko <anastasia.hrynkevich@outlook.com>
2022-04-12 00:39:54 +0100

    Change logo.

--- web_app.js
+++ web_app.js
@@ -19,24 +19,50 @@ $('.nav-buttons .next').click(function () {
     }
 });*/
 
-$(document).ready(function () {
-    $('.nav-buttons .next').click(function () {
-        let elNow = $('.form-block.show');
-        let elNext = elNow.next();
-        if (elNext && elNext.length) {
-            elNow.removeClass("show");
-            elNext.addClass("show");
-        }
-    });
-    $('.nav-buttons .prev').click(function () {
-        let elNow = $('.form-block.show');
-        let elPrev = elNow.prev();
-        if (elPrev && elPrev.length) {
-            elNow.removeClass('show');
-            elPrev.addClass('show');
-        }
-    });
+class Menu {
+    makeSticky() {
+        $(window).scroll(function () {
+            if ($(document).scrollTop() > 50) {
+                // make nav sticky
+                $(".nav-scroll").addClass("scroll-sticky");
+                // swap nav bar color
+                $(".nav-web").addClass("nav-sticky");
+                // swap logo text background
+                $(".logo img:eq(0)").removeClass("visible");
+                $(".logo img:eq(1)").addClass("visible");
+            } else {
+                // remove sticky nav bar
+                $(".nav-scroll").removeClass("scroll-sticky");
+                // remove sticky from nav
+                $(".nav-web").removeClass("nav-sticky");
+                // swap logo to black text
+                $(".logo img:eq(0)").addClass("visible");
+                $(".logo img:eq(1)").removeClass("visible");
+            }
+        });
+    }
+}
 
-});
+class Calendar {
+    initNext() {
+        $('.nav-buttons .next').click(function () {
+            let elNow = $('.form-block.show');
+            let elNext = elNow.next();
+            if (elNext && elNext.length) {
+                elNow.removeClass("show");
+                elNext.addClass("show");
+            }
+        });
+    }
 
-
+    initPrev() {
+        $('.nav-buttons .prev').click(function () {
+            let elNow = $('.form-block.show');
+            let elPrev = elNow.prev();
+            if (elPrev && elPrev.length) {
+                elNow.removeClass('show');
+                elPrev.addClass('show');
+            }
+        });
+    }
+}
