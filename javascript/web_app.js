f223d84817541118ff02bd32ff84b9d025c4b648
nastiko <anastasia.hrynkevich@outlook.com>
2022-05-14 22:43:47 +0100

    Double-checked all code, tested 3 pages on bugs

--- javascript/web_app.js
+++ javascript/web_app.js
@@ -1,24 +1,3 @@
-/*let currentStep = $('.form-block.show').index()+1;
-$('.nav-buttons .next').click(function () {
-    let elNow = $('.form-block.show');
-    let elNext = elNow.next();
-    if (elNext && elNext.length) {
-        elNow.removeClass("show");
-        elNext.addClass("show");
-        $('.calendar-info strong').html(`${++currentStep}/3`);
-    } else if (elNext && elNext.length) {
-        elNow.removeClass("show");
-        elNext.addClass("submit-form");
-
-    }
-
-
-
-    else {
-        $(this).prop('submit');
-    }
-});*/
-
 class Menu {
     makeSticky() {
         $(window).scroll(function () {
@@ -89,7 +68,7 @@ class MobileMenu {
     }
 
     slideRight() {
-        $('.closebtn').click (function () {
+        $('.icon-x').click (function () {
             $("#mySidenav").animate({
                 width: '0%'
             }, "fast");
