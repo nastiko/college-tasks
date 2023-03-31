ad89fbd1315f67e7427b6518ac44cd8bb7457e75
nastiko <anastasia.hrynkevich@outlook.com>
2023-03-31 15:32:47 +0100

    init: - created validation form       - added content on server

--- javascript/web_app.js
+++ javascript/web_app.js
@@ -33,9 +33,321 @@ class Header {
 }
 
 class Calendar {
+    initMonth = [
+        'January', 'February', 'March',
+        'April', 'May', 'June', 'July',
+        'August', 'September', 'October',
+        'November', 'December'
+    ];
+
+    #days;
+    #month;
+    #year;
+    #leftArrow;
+    #rightArrow;
+
+    #date = new Date();
+    #selectedDate = {};
+
+    constructor(daysId, dayMonthId, yearId, leftArrow, rightArrow) {
+        this.#days = document.getElementById(daysId);
+        this.#month = document.getElementById(dayMonthId);
+        this.#year = document.getElementById(yearId);
+        this.#leftArrow = document.getElementById(leftArrow);
+        this.#rightArrow = document.getElementById(rightArrow);
+    }
+
+    getDays() {
+        let days = '';
+
+        for (let k = this.getFirstDayIndex(); k > 0; k--) {
+            days += `<div class="prev-date">${this.getPrevDays() - k + 1}</div>`;
+        }
+
+        for (let i = 1; i <= this.getLastDay(); i++) {
+            let elClass = '';
+
+            // check if current date is currently rendering element
+            if (i === new Date().getDate() && this.#date.getMonth() === new Date().getMonth()) {
+                elClass = "today";
+            }
+
+            // check if saved date is currently rendered element
+            if (i === this.#selectedDate.day && this.#date.getMonth() === this.#selectedDate.month) {
+                elClass += " clicked";
+            }
+
+            days += `<div class="${elClass}" data-month="${this.#date.getMonth()}" data-day="${i}" data-year="${this.#date.getFullYear()}">${i}</div>`;
+        }
+
+        for (let j = 1; j <= this.getNextDays(); j++) {
+            days += `<div class="next-date">${j}</div>`;
+            this.#days.innerHTML = days;
+        }
+    }
+
+    getLastDay() {
+        return new Date(this.#date.getFullYear(), this.#date.getMonth() + 1, 0).getDate();
+    }
+
+    getPrevDays() {
+        return new Date(this.#date.getFullYear(), this.#date.getMonth(), 0).getDate();
+    }
+
+    getNextDays() {
+        return 7 - this.getLastDayIndex();
+    }
+
+    // get index of the first day of the month
+    getFirstDayIndex() {
+        return new Date(this.#date.getFullYear(), this.#date.getMonth(), 0).getDay();
+    }
+
+    getLastDayIndex() {
+        return new Date(this.#date.getFullYear(), this.#date.getMonth() + 1, 0).getDay();
+    }
+
+    getDayMonth() {
+        this.#month.innerHTML = `${(this.initMonth[this.#date.getMonth()]).toUpperCase()}`;
+    }
+
+    getYear() {
+        this.#year.innerHTML = `${this.#date.getFullYear()}`;
+    }
+
+    getPrevMonth() {
+        this.#date.setMonth(this.#date.getMonth() - 1);
+        this.update();
+    }
+
+    getNextMonth() {
+        this.#date.setMonth(this.#date.getMonth() + 1);
+        this.update();
+    }
+
+    addClickAction() {
+        let cells = document.querySelectorAll('#days div:not(.prev-date,.next-date)');
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].addEventListener('click', (event) => this.clickHandler(event));
+        }
+    }
+
+    clickHandler(event) {
+        // visually mark element as clicked
+        document.querySelectorAll('#days div.clicked').forEach((el) => {
+            el.classList.remove('clicked');
+        });
+        event.target.classList.add('clicked');
+
+        // save selected date
+        this.#selectedDate.day = parseInt(event.target.dataset.day);
+        this.#selectedDate.month = parseInt(event.target.dataset.month);
+        this.#selectedDate.year = parseInt(event.target.dataset.year);
+    }
+
+    init() {
+        this.#leftArrow.addEventListener('click', () => this.getPrevMonth());
+        this.#rightArrow.addEventListener('click', () => this.getNextMonth());
+
+        this.update();
+    }
+
+    update() {
+        this.getDays();
+        this.getLastDay();
+        this.getDayMonth();
+        this.getYear();
+        this.addClickAction();
+    }
+}
+
+let calendar = new Calendar('days', 'day-month', 'year', 'left-arrow', 'right-arrow');
+calendar.init();
+
+// class SelectTimeGuest {
+//
+//     /*#time;
+//
+//     constructor(time) {
+//         this.#time = time;
+//     }*/
+//
+//     selectTime(event) {
+//         document.querySelectorAll('#select-time tr .clicked').forEach((el) => {
+//             el.classList.remove('clicked');
+//         });
+//         event.target.classList.add('clicked');
+//     }
+//
+//     clickAction() {
+//         let cells = document.querySelectorAll('#select-time tr');
+//         for (let i = 0; i < cells.length; i++) {
+//             cells[i].addEventListener('click', (event) => this.selectTime(event));
+//         }
+//     }
+// }
+//
+// let timeGuest = new SelectTimeGuest();
+// timeGuest.clickAction();
+
+
+class Form {
+
+    #fullName;
+    #email;
+    #phone;
+    #message;
+    #checkbox;
+
+    #validationResult = true;
+
+    constructor(fullName, email, phone, message, checkbox) {
+        this.#fullName = fullName;
+        this.#email = email;
+        this.#phone = phone;
+        this.#message = message;
+        this.#checkbox = checkbox;
+    }
+
+    getFullNameValue(){
+        return this.#fullName.value.trim();
+    }
+
+    getEmailValue() {
+        return this.#email.value.trim();
+    }
+
+    getPhoneValue() {
+        return this.#phone.value.trim();
+    }
+
+    getMessageValue() {
+        return this.#message.value.trim();
+    }
+
+    getCheckboxValue() {
+        return this.#checkbox.value.trim();
+    }
+
+    setError(elem, message) {
+        let inputControl = elem.parentElement;
+        let errorDisplay = inputControl.querySelector('.error');
+
+        errorDisplay.innerHTML = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success');
+
+        this.#validationResult = false;
+    }
+
+    setSuccess(elem) {
+        let inputControl = elem.parentElement;
+        let errorDisplay = inputControl.querySelector('.error');
+
+        errorDisplay.innerHTML = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error');
+    }
+
+    isValidEmail(email) {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    }
+
+    isValidPhone(phone) {
+        return String(phone)
+            .match(
+                /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/);
+    }
+
+    validationInput() {
+
+        if (this.getFullNameValue() === '') {
+            this.setError(this.#fullName, 'Name is required');
+        } else {
+            this.setSuccess(this.#fullName);
+        }
+
+        if (this.getEmailValue() === '') {
+            this.setError(this.#email, 'Email is required');
+        } else if (!this.isValidEmail(this.#email)) {
+            this.setError(this.#email, 'Provide a valid email address');
+        } else {
+            this.setSuccess(this.#email);
+        }
+
+        if (this.getPhoneValue() === '') {
+            this.setError(this.#phone, 'Phone is required');
+        } else if (!this.isValidPhone(this.#phone)) {
+            this.setError(this.#phone, 'Provide a valid phone number');
+        } else {
+            this.setSuccess(this.#phone);
+        }
+
+        if (this.getMessageValue() === '') {
+            this.setError(this.#message, 'Message is required');
+        } else {
+            this.setSuccess(this.#message);
+        }
+
+        /*if (!this.#checkbox.checkbox.checked) {
+            this.setError(this.getCheckboxValue(), 'You must agree to the terms first');
+        }*/
+
+        return this.#validationResult;
+    }
+
+    handleSubmit() {
+        //event.preventDefault();
+
+        //validate? true / false
+        if(!validateForm.validationInput()){
+            return;
+        }
+
+        let blockSubmit = document.getElementById('btn-submit');
+        let status = document.createElement('div');
+        status.id = 'form-status';
+        status.classList.add('submit-style');
+        blockSubmit.appendChild(status);
+
+        let data = new FormData(event.target);
+        fetch(event.target.action, {
+            method: this.submitForm.method,
+            body: data
+        }).then(response => {
+            if (response.ok) {
+                status.innerHTML = 'Thanks for your submission!';
+                this.submitForm.reset();
+            } else {
+                status.innerHTML = 'Oops! There was a problem submitting your form';
+            }
+        }).catch(error => {
+            status.innerHTML = 'Oops! There was a problem submitting your form';
+        });
+    }
+
+    init(fullNameId, emailId, phoneId, messageId, checkboxId, formId) {
+        this.#fullName = document.getElementById(fullNameId);
+        this.#email = document.getElementById(emailId);
+        this.#phone = document.getElementById(phoneId);
+        this.#message = document.getElementById(messageId);
+        this.#checkbox = document.getElementById(checkboxId);
+        document.querySelector(formId).addEventListener('click', () => this.handleSubmit());
+    }
+}
+
+let validateForm = new Form();
+
+validateForm.init('fullName', 'email', 'phone', 'message', 'checkbox', '#form-submit .submit');
+
+
+class NextPrevBtn {
     initNext() {
         $('.button-next .next').click(function () {
-            let elNow  = $('.form-block.show');
+            let elNow = $('.form-block.show');
             let elNext = elNow.next();
             if (elNext && elNext.length) {
                 elNow.removeClass("show");
@@ -46,7 +358,7 @@ class Calendar {
 
     initPrev() {
         $('.button-next .prev').click(function () {
-            let elNow  = $('.form-block.show');
+            let elNow = $('.form-block.show');
             let elPrev = elNow.prev();
             if (elPrev && elPrev.length) {
                 elNow.removeClass('show');
@@ -83,13 +395,13 @@ class Menu {
 }
 
 class Slider {
-    slides            = $('#slider-content .slide');
-    slideWidth        = this.slides.outerWidth(true);
-    slideMaxWidth     = this.slideWidth * this.slides.length;
-    sliderViewport    = $('.slider-viewport');
-    sliderBlock       = $('.slick-slider');
-    sliderContent     = $('#slider-content');
-    currPosition      = 0;
+    slides = $('#slider-content .slide');
+    slideWidth = this.slides.outerWidth(true);
+    slideMaxWidth = this.slideWidth * this.slides.length;
+    sliderViewport = $('.slider-viewport');
+    sliderBlock = $('.slick-slider');
+    sliderContent = $('#slider-content');
+    currPosition = 0;
     animationDuration = 1000;
 
     getVisibleCount() {
---
+++ send_mail.php
@@ -0,0 +1,13 @@
+<?php
+if ($_POST['fullName'] && $_POST['email'] && $_POST['phone'] && $_POST['message']) {
+    mail(
+        'jogarat367@marikuza.com',
+        'Website form submission from `' . htmlspecialchars($_POST['fullName']) . ' (' . htmlspecialchars($_POST['email']) . ')',
+        htmlspecialchars($_POST['phone']) . htmlspecialchars($_POST['message'])
+    );
+    header("HTTP/1.1 200 OK");
+    echo 'Thank you, you for has been submitted';
+} else {
+    header("HTTP/1.1 500 Server Error");
+    echo 'Please check your form submission';
+}
